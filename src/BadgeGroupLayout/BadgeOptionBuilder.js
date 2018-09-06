638240ca2e481171b152e6b2eed82e601354d392
Noam Neeman <noamn@wix.com>
2018-09-06 16:36:02 +0300

    update builder to use Text component

--- src/BadgeGroupLayout/BadgeOptionBuilder.js
+++ src/BadgeGroupLayout/BadgeOptionBuilder.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import styles from './BadgeOption.st.css';
 import PropTypes from 'prop-types';
+import Text from '../Text/Text';
 
-const badgeOption = ({text, skin}) => (
-  <div {...styles('root', {skin}, {skin})}>
+const badgeOption = props => {
+  const {skin, text} = props;
+  return (<div {...styles('root', {skin}, props)}>
     <div className={styles.marker}/>
-    <span className={styles.label}>{text}</span>
-  </div>
-);
+    <Text
+      size="small"
+      skin="standard"
+      tagName="span"
+      weight="normal"
+      >
+      {text}
+    </Text>
+  </div>);
+};
 
 badgeOption.propTypes = {
   id: PropTypes.string.isRequired,

d43ce201b062993783ab97621c5d8364c050804a
yuriib <yuriib@wix.com>
2018-09-07 12:01:12 +0300

    Fix dropdownLayout in BadgeGroup

--- src/BadgeGroup/BadgeGroup.js
+++ src/BadgeGroup/BadgeGroup.js
@@ -2,24 +2,23 @@ import React from 'react';
 import DropdownLayout from '../DropdownLayout';
 import PropTypes from 'prop-types';
 import map from 'lodash/map';
-import {badgeOptionBuilder} from './BadgeOptionBuilder';
+import { badgeOptionBuilder } from './BadgeOptionBuilder';
 import styles from './BadgeGroup.scss';
 import noop from 'lodash/noop';
 import Badge from '../Badge/Badge';
-import {ChevronDown} from 'wix-ui-icons-common';
-import {SKIN, SIZE, TYPE} from '../../../wix-ui-backoffice/dist/src/components/Badge/constants';
+import { ChevronDown } from 'wix-ui-icons-common';
 
 export default class BadgeGroup extends React.Component {
   static propTypes = {
     options: PropTypes.arrayOf(PropTypes.shape({
       id: PropTypes.string,
-      skin: PropTypes.oneOf(Object.keys(SKIN)),
+      skin: PropTypes.string,
       text: PropTypes.string
     })),
     selectedId: PropTypes.string,
     onSelect: PropTypes.func,
-    size: PropTypes.oneOf(Object.keys(SIZE)),
-    type: PropTypes.oneOf(Object.keys(TYPE)),
+    size: PropTypes.string,
+    type: PropTypes.string,
     uppercase: PropTypes.bool
   };
 
@@ -31,60 +30,55 @@ export default class BadgeGroup extends React.Component {
     super(props);
     this.state = {
       visible: false,
-      selectedBadge: props.options.find(({id}) => id === props.selectedId)
+      selectedBadge: props.options.find(({ id }) => id === props.selectedId)
     };
   }
 
-  componentWillReceiveProps({selectedId}) {
+  componentWillReceiveProps({ selectedId }) {
     if (this.props.selectedId !== selectedId) {
       this.setState({
-        selectedBadge: this.props.options.find(({id}) => id === selectedId)
+        selectedBadge: this.props.options.find(({ id }) => id === selectedId)
       });
     }
   }
 
   get options() {
-    const {options} = this.props;
+    const { options } = this.props;
     return map(options, badgeOptionBuilder);
   }
 
-  toggleDropdown = () => {
-    this.setState({visible: !this.state.visible});
+  toggleDropdown() {
+    this.setState({ visible: !this.state.visible });
   }
 
-  handleSelect = ({id: selectedId}) =>{
-    const selectedBadge = this.props.options.find(({id}) => id === selectedId);
-    this.setState({selectedBadge});
+  handleSelect({ id: selectedId }) {
+    const selectedBadge = this.props.options.find(({ id }) => id === selectedId);
+    this.setState({ selectedBadge });
     this.props.onSelect(selectedBadge);
-    this.setState({visible: false});
+    this.setState({ visible: false });
   }
 
   render() {
-    const {type, size, uppercase} = this.props;
+    const { type, size, uppercase } = this.props;
 
     return (
-      <div>
-        <div className={styles.badge_holder}>
-          <div className={styles.badge}>
-            <Badge
-              {...{type, size, uppercase}}
-              suffixIcon={<ChevronDown/>}
-              onClick={this.toggleDropdown}
-              skin={this.state.selectedBadge.skin}
-              >
-              {this.state.selectedBadge.text}
-            </Badge>
-          </div>
-        </div>
-        <div className={styles.container}>
+      <div className={styles.container}>
+        <Badge
+          {...{ type, size, uppercase }}
+          suffixIcon={<ChevronDown/>}
+          onClick={this.toggleDropdown.bind(this)}
+          skin={this.state.selectedBadge.skin}>
+          {this.state.selectedBadge.text}
+        </Badge>
+        <div className={styles.dropdown}>
           <DropdownLayout
             visible={this.state.visible}
             selectedId={this.state.selectedBadge.id}
             options={this.options}
-            onSelect={this.handleSelect}
+            onSelect={this.handleSelect.bind(this)}
             isInContainer
-            onClickOutside={()=>this.setState({visible: false})}
-            />
+            onClickOutside={() => this.setState({ visible: false })}
+          />
         </div>
       </div>
     );

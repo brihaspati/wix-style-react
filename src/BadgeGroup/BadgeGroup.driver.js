0977f83175b3f21bb8054052cc52ae21ddf48730
Noam Neeman <noamn@wix.com>
2018-09-07 16:03:03 +0300

    added badgegroup driver

---
+++ src/BadgeGroup/BadgeGroup.driver.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import dropdownLayoutDriverFactory from '../DropdownLayout/DropdownLayout.driver';
+import {badgeDriverFactory} from 'wix-ui-backoffice/dist/src/components/Badge/Badge.driver';
+import ReactDOM from 'react-dom';
+
+const badgeGroupDriverFactory = ({element, wrapper, component}) => {
+  const badgeWrapper = element.querySelector('[data-hook=badgeGroup-badge-wrapper]');
+  const dropdownLayout = element.querySelector('[data-hook=badgeGroup-dropdownLayout]');
+  const badgeDriver = badgeDriverFactory({element: badgeWrapper.childNodes[0], wrapper: badgeWrapper});
+  const dropdownLayoutDriver = dropdownLayoutDriverFactory({element: dropdownLayout, wrapper});
+
+  const driver = {
+    exists: () => !!element,
+    outsideClick: () => document.body.dispatchEvent(new Event('mouseup', {cancelable: true})),
+    setProps: props => {
+      const ClonedWithProps = React.cloneElement(component, Object.assign({}, component.props, props), ...(component.props.children || []));
+      ReactDOM.render(<div ref={r => element = r}>{ClonedWithProps}</div>, wrapper);
+    }
+  };
+
+  return {driver, badgeDriver, dropdownLayoutDriver};
+};
+
+export default badgeGroupDriverFactory;
